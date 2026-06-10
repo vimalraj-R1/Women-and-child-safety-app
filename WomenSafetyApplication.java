04b259eca6c69fc5cbf75942786d6b322b62175d
VIMALRAJ.R <vimalraja9973@gmail.com>
2026-06-10 15:32:25 +0530

    Create WomenSafetyApplication.java

---
+++ WomenSafetyApplication.java
@@ -0,0 +1,16 @@
+package com.rabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomenSafetyApplication {
+
+public static void main(String[] args) {
+SpringApplication.run(
+WomenSafetyApplication.class,
+args
+);
+}
+
+}
